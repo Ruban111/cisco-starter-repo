38acc917406d8efc2786705e89eba5d679091f41
Ruban111 <129514333+Ruban111@users.noreply.github.com>
2024-06-01 21:12:40 +1000

    Added Banner to Dashboard

---
+++ sextant/src/Components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Banner = () =>{
+
+return(
+    <>
+    <div style={{backgroundColor: 'black', color: 'white', height: '5%', padding: '30px'}}>
+        <div className="title" style={{marginLeft: '0%', textAlign: 'left', fontWeight: 'bold', fontSize: 30}}>Sextant</div>
+    </div>
+    </>
+);
+
+}
+
+export default Banner;
---
+++ sextant/src/Pages/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from '../Components/Banner';
+
+
+const Dashboard = () =>{
+
+return(<>
+<div>
+    <Header />
+</div>
+
+</>
+);
+
+};
+
+export default Dashboard;
